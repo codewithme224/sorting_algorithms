dc8b91d2e7de62f7f280c7c10045aa6f69dae1c3
codewithme224 <codewithme224@gmail.com>
2023-04-21 08:32:51 -0700

    Quick sort algorithm

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -56,23 +56,29 @@ int partition(int *array, int low, int high, size_t size)
 	int pivot, i, j, temp;
 
 	pivot = array[high];
-	i = low - 1;
-	for (j = low; j <= high - 1; j++)
+	i = low;
+
+	for (j = low; j < high; j++)
 	{
-		if (array[j] <= pivot)
+		if (array[j] < pivot)
 		{
-			i++;
-			temp = array[i];
-			array[i] = array[j];
-			array[j] = temp;
 			if (i != j)
+			{
+				temp = array[j];
+				array[j] = array[i];
+				array[i] = temp;
 				print_array(array, size);
+			}
+			i++;
 		}
 	}
-	temp = array[i + 1];
-	array[i + 1] = array[high];
-	array[high] = temp;
-	if (i + 1 != high)
+	if (array[i] != array[high])
+	{
+		temp = array[i];
+		array[i] = array[high];
+		array[right] = temp;
 		print_array(array, size);
-	return (i + 1);
+	}
+
+	return (i);
 }
