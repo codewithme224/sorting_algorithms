13c1f6df1722d7555b22dbd3e5c347d098994259
codewithme224 <codewithme224@gmail.com>
2023-04-22 19:42:29 -0700

    Counting sort algorithm

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -10,38 +10,46 @@
 
 void counting_sort(int *array, size_t size)
 {
-	int *count, *output;
-	size_t i, max = 0;
+	int *count, *output, max_value, i;
 
-	if (array == NULL || size < 2)
+	if (!array || size < 2)
 		return;
 
-	for (i = 0; i < size; i++)
-	{
-		if (array[i] > (int)max)
-			max = array[i];
-	}
-	count = malloc(sizeof(int) * (max + 1));
-	if (count == NULL)
+	max_value = array[0];
+	for (i = 1; i < (int)size; i++)
+		if (array[i] > max_value)
+			max_value = array[i];
+
+	count = malloc(sizeof(int) * (max_value + 1));
+	if (!count)
 		return;
+
 	output = malloc(sizeof(int) * size);
-	if (output == NULL)
+	if (!output)
+	{
+		free(count);
 		return;
+	}
 
-	for (i = 0; i <= max; i++)
+	for (i = 0; i <= max_value; i++)
 		count[i] = 0;
-	for (i = 0; i < size; i++)
+
+	for (i = 0; i < (int)size; i++)
 		count[array[i]]++;
-	for (i = 1; i <= max; i++)
+
+	for (i = 1; i <= max_value; i++)
 		count[i] += count[i - 1];
-	print_array(count, max + 1);
-	for (i = 0; i < size; i++)
+	print_array(count, max_value + 1);
+
+	for (i = 0; i < (int)size; i++)
 	{
 		output[count[array[i]] - 1] = array[i];
 		count[array[i]]--;
 	}
-	for (i = 0; i < size; i++)
+
+	for (i = 0; i < (int)size; i++)
 		array[i] = output[i];
+
 	free(count);
 	free(output);
 }
