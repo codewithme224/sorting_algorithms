8abdb04b7e7a1e5894ce6b95c3b2dfb3dbe0288c
codewithme224 <codewithme224@gmail.com>
2023-04-23 07:57:09 -0700

    Deck card

---
+++ 1000-sort_deck.c
@@ -0,0 +1,62 @@
+#include "deck.h"
+
+/**
+ * swap - swaps two nodes in a doubly linked list
+ * @deck: pointer to head of list
+ * @node1: first node
+ * @node2: second node
+ */
+
+void swap(deck_node_t **deck, deck_node_t *node1, deck_node_t *node2)
+{
+	deck_node_t *tmp;
+
+	if (node1->prev)
+		node1->prev->next = node2;
+	else
+		*deck = node2;
+	if (node2->next)
+		node2->next->prev = node1;
+	node1->next = node2->next;
+	node2->prev = node1->prev;
+	node1->prev = node2;
+	node2->next = node1;
+
+	tmp = node1;
+	node1 = node2;
+	node2 = tmp;
+}
+
+/**
+ * sort_deck - sorts a deck of cards
+ * @deck: pointer to head of list
+ */
+
+void sort_deck(deck_node_t **deck)
+{
+	deck_node_t *node1, *node2;
+	int i, j;
+
+	if (!deck || !(*deck) || !(*deck)->next)
+		return;
+	node1 = *deck;
+	while (node1)
+	{
+		node2 = node1->next;
+		while (node2)
+		{
+			i = 0;
+			j = 0;
+
+			while (node1->card->value[i] != '\0')
+				i++;
+			while (node2->card->value[j] != '\0')
+				j++;
+			if (i > j || (i == j && node1->card->kind > node2->card->kind))
+				swap(deck, node1, node2);
+			else
+				node2 = node2->next;
+		}
+		node1 = node1->next;
+	}
+}
