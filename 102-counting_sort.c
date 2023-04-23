b8078c8177787dd6c5ade9ab252d157c906e4e6f
codewithme224 <codewithme224@gmail.com>
2023-04-22 20:00:49 -0700

    Counting sort algorithm

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -10,37 +10,39 @@
 
 void counting_sort(int *array, size_t size)
 {
-	int n, j, *count_array, *aux;
-	size_t i;
+	int *count, *output, max_value, i;
 
 	if (!array || size < 2)
 		return;
-	n = array[0];
-	for (i = 0; i < size; i++)
+	max_value = array[0];
+	for (i = 1; i < (int)size; i++)
+		if (array[i] > max_value)
+			max_value = array[i];
+	count = malloc(sizeof(int) * (max_value + 1));
+	if (!count)
+		return;
+	output = malloc(sizeof(int) * size);
+	if (!output)
 	{
-		if (array[i] > n)
-			n = array[i];
+		free(count);
+		return;
 	}
-	count_array = calloc((n + 1), sizeof(int));
-	for (i = 0; i < size; i++)
+
+	for (i = 0; i <= max_value; i++)
+		count[i] = 0;
+	for (i = 0; i < (int)size; i++)
+		count[array[i]]++;
+	for (i = 1; i <= max_value; i++)
+		count[i] += count[i - 1];
+	print_array(count, max_value + 1);
+	for (i = 0; i < (int)size; i++)
 	{
-		count_array[array[i]]++;
+		output[count[array[i]] - 1] = array[i];
+		count[array[i]]--;
 	}
-	for (j = 1; j < n; j++)
-	{
-		count_array[j + 1] += count_array[j];
-	}
-	print_array(count_array, n + 1);
-	aux = malloc(sizeof(int) * size);
-	for (i = 0; i < size; i++)
-	{
-		count_array[array[i]]--;
-		aux[count_array[array[i]]] = array[i];
-	}
-	for (i = 0; i < size; i++)
-	{
-		array[i] = aux[i];
-	}
-	free(aux);
-	free(count_array);
+	for (i = 0; i < (int)size; i++)
+		array[i] = output[i];
+
+	free(count);
+	free(output);
 }
