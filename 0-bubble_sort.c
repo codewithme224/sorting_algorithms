cff015ac47d4db56a4e41104484f90bd96ba005c
codewithme224 <codewithme224@gmail.com>
2023-04-25 12:55:19 -0700

    Bubble Sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,17 +10,19 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
+	int tmp;
 
-	for (i = 0; i < size - 1; i++)
+	if (!array || size < 2)
+		return;
+	for (i = 0; i < size -1; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
-				int temp = array[j];
-
+				tmp = array[j];
 				array[j] = array[j + 1];
-				array[j + 1] = temp;
+				array[j + 1] = tmp;
 				print_array(array, size);
 			}
 		}
